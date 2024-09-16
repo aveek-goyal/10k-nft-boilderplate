a25ff80bcfb3d3b5ff81eb5d78dfc2ecc24f2f86
aveek-goyal <aveekgoel2011@gmail.com>
2024-09-16 11:34:35 +0530

    code update

--- backend/ignition/modules/NFT.js
+++ backend/ignition/modules/NFT.js
@@ -1,10 +1,8 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-
-
 module.exports = buildModule("NFTModule", (m) => {
 
-  const nft = m.contract("CoreNFT", ["https://corenft.vercel.app/generated_metadata/"]);
+  const nft = m.contract("CoreNFT", ["https://aveek-goyal.github.io/10k-nft-collection/generated_metadata/"]);
 
   return { nft };
 });
